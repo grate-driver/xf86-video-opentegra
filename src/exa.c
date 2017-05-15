095585e116a7bfbb2e75337af66140fb622bd7be
Dmitry Osipenko <digetx@gmail.com>
2017-05-15 20:03:34 +0300

    exa: reduce commands buffer size by one
    The gather maximum words count is 16383, not 16384.

--- src/exa.c
+++ src/exa.c
@@ -486,7 +486,7 @@ void TegraEXAScreenInit(ScreenPtr pScreen)
         goto free_priv;
     }
 
-    err = tegra_stream_create(tegra->drm, priv->gr2d, &priv->cmds, 16384);
+    err = tegra_stream_create(tegra->drm, priv->gr2d, &priv->cmds, 16383);
     if (err < 0) {
         ErrorMsg("failed to create command stream: %d\n", err);
         goto close_gr2d;
