0207a79e785b75856ab7dda4f96af27791c9a110
Dmitry Osipenko <digetx@gmail.com>
2018-12-21 22:57:29 +0300

    xv: support non-blocking atomic commit
    Add new "VDPAU" API that doesn't block and can be further extended with support
    of framebuffer modifiers.

--- src/xv.h
+++ src/xv.h
@@ -35,6 +35,13 @@
 #define FOURCC_PASSTHROUGH_XRGB8888 (('X' << 24) + ('B' << 16) + ('G' << 8) + 'R')
 #define FOURCC_PASSTHROUGH_XBGR8888 (('X' << 24) + ('R' << 16) + ('G' << 8) + 'B')
 
+#define PASSTHROUGH_DATA_SIZE_V2 128
+
+#define FOURCC_PASSTHROUGH_YV12_V2     (('T' << 24) + ('G' << 16) + ('R' << 8) + '1')
+#define FOURCC_PASSTHROUGH_RGB565_V2   (('T' << 24) + ('G' << 16) + ('R' << 8) + '2')
+#define FOURCC_PASSTHROUGH_XRGB8888_V2 (('T' << 24) + ('G' << 16) + ('R' << 8) + '3')
+#define FOURCC_PASSTHROUGH_XBGR8888_V2 (('T' << 24) + ('G' << 16) + ('R' << 8) + '4')
+
 #define XVMC_YV12                                   \
 {                                                   \
     .id                 = FOURCC_PASSTHROUGH_YV12,  \
@@ -125,6 +132,118 @@
     .type               = XvRGB,                    \
     .byte_order         = LSBFirst,                 \
     .guid               = {'P', 'A', 'S', 'S', 'T', 'H', 'R', 'O', 'U', 'G', 'H', 'B', 'G', 'R', '3', '2'}, \
+    .bits_per_pixel     = 32,                       \
+    .format             = XvPacked,                 \
+    .num_planes         = 1,                        \
+    /* for RGB formats only */                      \
+    .depth              = 24,                       \
+    .red_mask           = 0xff <<  0,               \
+    .green_mask         = 0xff <<  8,               \
+    .blue_mask          = 0xff << 16,               \
+    /* for YUV formats only */                      \
+    .y_sample_bits      = 0,                        \
+    .u_sample_bits      = 0,                        \
+    .v_sample_bits      = 0,                        \
+    .horz_y_period      = 0,                        \
+    .horz_u_period      = 0,                        \
+    .horz_v_period      = 0,                        \
+    .vert_y_period      = 0,                        \
+    .vert_u_period      = 0,                        \
+    .vert_v_period      = 0,                        \
+    .component_order    = {'R', 'G', 'B', 'X', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, \
+    .scanline_order     = XvTopToBottom,            \
+}
+
+#define XVMC_YV12_V2                                \
+{                                                   \
+    .id                 = FOURCC_PASSTHROUGH_YV12_V2,\
+    .type               = XvYUV,                    \
+    .byte_order         = LSBFirst,                 \
+    .guid               = {'P', 'A', 'S', 'S', 'T', 'H', 'R', 'O', 'U', 'G', 'H', '_', 'T', 'G', 'R', '1'}, \
+    .bits_per_pixel     = 12,                       \
+    .format             = XvPlanar,                 \
+    .num_planes         = 3,                        \
+    /* for RGB formats only */                      \
+    .depth              = 0,                        \
+    .red_mask           = 0,                        \
+    .green_mask         = 0,                        \
+    .blue_mask          = 0,                        \
+    /* for YUV formats only */                      \
+    .y_sample_bits      = 8,                        \
+    .u_sample_bits      = 8,                        \
+    .v_sample_bits      = 8,                        \
+    .horz_y_period      = 1,                        \
+    .horz_u_period      = 2,                        \
+    .horz_v_period      = 2,                        \
+    .vert_y_period      = 1,                        \
+    .vert_u_period      = 1,                        \
+    .vert_v_period      = 2,                        \
+    .component_order    = {'Y', 'V', 'U', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, \
+    .scanline_order     = XvTopToBottom,            \
+}
+
+#define XVMC_RGB565_V2                              \
+{                                                   \
+    .id                 = FOURCC_PASSTHROUGH_RGB565_V2,\
+    .type               = XvRGB,                    \
+    .byte_order         = LSBFirst,                 \
+    .guid               = {'P', 'A', 'S', 'S', 'T', 'H', 'R', 'O', 'U', 'G', 'H', '_', 'T', 'G', 'R', '2'}, \
+    .bits_per_pixel     = 16,                       \
+    .format             = XvPacked,                 \
+    .num_planes         = 1,                        \
+    /* for RGB formats only */                      \
+    .depth              = 16,                       \
+    .red_mask           = 0x1f << 11,               \
+    .green_mask         = 0x3f <<  5,               \
+    .blue_mask          = 0x1f <<  0,               \
+    /* for YUV formats only */                      \
+    .y_sample_bits      = 0,                        \
+    .u_sample_bits      = 0,                        \
+    .v_sample_bits      = 0,                        \
+    .horz_y_period      = 0,                        \
+    .horz_u_period      = 0,                        \
+    .horz_v_period      = 0,                        \
+    .vert_y_period      = 0,                        \
+    .vert_u_period      = 0,                        \
+    .vert_v_period      = 0,                        \
+    .component_order    = {'B', 'G', 'R', 'X', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, \
+    .scanline_order     = XvTopToBottom,            \
+}
+
+#define XVMC_XRGB8888_V2                            \
+{                                                   \
+    .id                 = FOURCC_PASSTHROUGH_XRGB8888_V2,\
+    .type               = XvRGB,                    \
+    .byte_order         = LSBFirst,                 \
+    .guid               = {'P', 'A', 'S', 'S', 'T', 'H', 'R', 'O', 'U', 'G', 'H', '_', 'T', 'G', 'R', '3'}, \
+    .bits_per_pixel     = 32,                       \
+    .format             = XvPacked,                 \
+    .num_planes         = 1,                        \
+    /* for RGB formats only */                      \
+    .depth              = 24,                       \
+    .red_mask           = 0xff << 16,               \
+    .green_mask         = 0xff <<  8,               \
+    .blue_mask          = 0xff <<  0,               \
+    /* for YUV formats only */                      \
+    .y_sample_bits      = 0,                        \
+    .u_sample_bits      = 0,                        \
+    .v_sample_bits      = 0,                        \
+    .horz_y_period      = 0,                        \
+    .horz_u_period      = 0,                        \
+    .horz_v_period      = 0,                        \
+    .vert_y_period      = 0,                        \
+    .vert_u_period      = 0,                        \
+    .vert_v_period      = 0,                        \
+    .component_order    = {'B', 'G', 'R', 'X', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, \
+    .scanline_order     = XvTopToBottom,            \
+}
+
+#define XVMC_XBGR8888_V2                            \
+{                                                   \
+    .id                 = FOURCC_PASSTHROUGH_XBGR8888_V2,\
+    .type               = XvRGB,                    \
+    .byte_order         = LSBFirst,                 \
+    .guid               = {'P', 'A', 'S', 'S', 'T', 'H', 'R', 'O', 'U', 'G', 'H', '_', 'T', 'G', 'R', '4'}, \
     .bits_per_pixel     = 32,                       \
     .format             = XvPacked,                 \
     .num_planes         = 1,                        \
