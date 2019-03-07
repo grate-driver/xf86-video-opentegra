fe1f448daadba1712d85ff8300ab39b47cce6db9
Dmitry Osipenko <digetx@gmail.com>
2019-03-07 18:52:33 +0300

    exa: composite-3d: optimize opOver tex-tex shader
    Optimize opOver shader variant that takes both src and mask as textures.

--- src/shaders/blend_over/blend_over/fragment.asm
+++ src/shaders/blend_over/blend_over/fragment.asm
@@ -20,20 +20,16 @@
  * DEALINGS IN THE SOFTWARE.
  */
 
-pseq_to_dw_exec_nb = 15	// the number of 'EXEC' block where DW happens
+pseq_to_dw_exec_nb = 3	// the number of 'EXEC' block where DW happens
 alu_buffer_size = 2	// number of .rgba regs carried through pipeline
 
 .uniforms
 	[5].l = "src_fmt_alpha";
-	[5].h = "src_swap_bgr";
 
 	[6].l = "mask_has_per_component_alpha";
 	[6].h = "mask_fmt_alpha";
-	[7].l = "mask_swap_bgr";
-	[7].h = "mask_clamp_to_border";
 
 	[8].l = "dst_fmt_alpha";
-	[8].h = "src_clamp_to_border";
 
 .asm
 
@@ -44,26 +40,6 @@ EXEC
 		mul1: bar, sfu, bar1
 		ipl: t0.fp20, t0.fp20, t0.fp20, t0.fp20
 
-	// Emulate clamp-to-border for mask
-	ALU:
-		ALU0:	MAD  lp.lh, r2, #1, -#1
-		ALU1:	MAD  lp.lh, r3, #1, -#1
-
-	ALU:
-		ALU0:	CSEL lp.lh,   r2, u7.h, #0 (this)
-		ALU1:	CSEL lp.lh, alu0, #0, u7.h (other)
-		ALU2:	CSEL lp.lh,   r3, u7.h, #0 (other)
-		ALU3:	CSEL lp.lh, alu1, #0, u7.h
-
-	ALU:
-		ALU0:	CSEL kill, alu0, #1, #0
-;
-
-EXEC
-;
-
-// Second batch
-EXEC
 	// sample tex1 (mask)
 	TEX:	tex r2, r3, tex1, r2, r3, r0
 
@@ -80,100 +56,22 @@ EXEC
 	// mask.b = mask_has_per_component_alpha ? mask.b : tmp
 	// mask.a = dst_fmt_alpha ? tmp : 0.0
 	ALU:
-		ALU0:	CSEL r2.l, -u6.l, alu1, alu0
-		ALU1:	CSEL r2.h, -u6.l, r2.h, alu0
-		ALU2:	CSEL r3.l, -u6.l, alu2, alu0
-		ALU3:	CSEL r3.h, -u8.l, alu0, #0
+		ALU0:	CSEL r4.l, -u6.l, alu1, alu0
+		ALU1:	CSEL r4.h, -u6.l, r2.h, alu0
+		ALU2:	CSEL r5.l, -u6.l, alu2, alu0
+		ALU3:	CSEL r5.h, -u8.l, alu0, #0
 ;
 
-EXEC
-;
-
-// Third batch
-EXEC
-	ALU:
-		ALU0:	MAD  lp.lh, r2.l, #1, #0 (this)
-		ALU1:	MAD  lp.lh, r2.h, #1, #0 (other)
-		ALU2:	MAD  lp.lh, r3.l, #1, #0 (other)
-		ALU3:	MAD  lp.lh, r3.h, #1, #0
-
-	// kill the pixel if mask is opaque
-	ALU:
-		ALU0:	CSEL kill, -alu0, #0, #1
-;
-
-EXEC
-	ALU:
-		ALU0:	MAD  r4.l, r2.l, #1, #0
-		ALU1:	MAD  r4.h, r2.h, #1, #0
-		ALU2:	MAD  r5.l, r3.l, #1, #0
-		ALU3:	MAD  r5.h, r3.h, #1, #0
-;
-
-// Fourth batch
 EXEC
 	// sample tex0 (src)
 	TEX:	tex r2, r3, tex0, r0, r1, r2
 
 	// src.a = src_fmt_alpha ? src.a : 1.0
 	ALU:
-		ALU0:	CSEL r7.h, -u5.l, r3.h, #1
-		ALU1:	MAD  r6.h, r2.h, #1, #0
-
-		// swap src ABGR to ARGB if needed
-		ALU2:	CSEL r6.l, -u5.h, r3.l, r2.l
-		ALU3:	CSEL r7.l, -u5.h, r2.l, r3.l
+		ALU0:	CSEL r7.h, -u5.l, r7.h, #1
 ;
 
-EXEC
-;
-
-// Fifth batch
-EXEC
-	// Emulate clamp-to-border for src
-	ALU:
-		ALU0:	MAD  lp.lh, r0, #1, -#1
-		ALU1:	MAD  lp.lh, r1, #1, -#1
-
-	ALU:
-		ALU0:	CSEL lp.lh,   r0, u8.h, #0 (this)
-		ALU1:	CSEL lp.lh, alu0, #0, u8.h (other)
-		ALU2:	CSEL lp.lh,   r1, u8.h, #0 (other)
-		ALU3:	CSEL lp.lh, alu1, #0, u8.h
-
-	ALU:
-		ALU0:	MAD  r6.l, r6.l, #1, -alu0 (sat)
-		ALU1:	MAD  r6.h, r6.h, #1, -alu0 (sat)
-		ALU2:	MAD  r7.l, r7.l, #1, -alu0 (sat)
-		ALU3:	MAD  r7.h, r7.h, #1, -alu0 (sat)
-;
-
-EXEC
-;
-
-// Sixth batch
-EXEC
-	ALU:
-		ALU0:	MAD  lp.lh, r6.l, r4.l, #0
-		ALU1:	MAD  lp.lh, r6.h, r4.h, #0
-		ALU2:	MAD  lp.lh, r7.l, r5.l, #0
-		ALU3:	MAD  lp.lh, r7.h, r5.h, r7.h
-
-	ALU:
-		ALU0:	MAD  lp.lh, alu0, #1, #0 (this)
-		ALU1:	MAD  lp.lh, alu1, #1, #0 (other)
-		ALU2:	MAD  lp.lh, alu2, #1, #0 (other)
-		ALU3:	MAD  lp.lh, alu3, #1, #0
-
-	// kill the pixel if src.bgra * mask.bgra == 0 && src.a == 0
-	ALU:
-		ALU0:	CSEL kill, -alu0, #0, #1
-;
-
-EXEC
-;
-
-// Seventh batch
+// Second batch
 EXEC
 	// fetch dst pixel to r2,r3
 	PSEQ:	0x0081000A
@@ -198,29 +96,6 @@ EXEC
 		ALU1:	MAD  r0.h, r6.h, r4.h, alu1 (sat)
 		ALU2:	MAD  r1.l, r7.l, r5.l, alu2 (sat)
 		ALU3:	MAD  r1.h, r7.h, r5.h, alu3 (sat)
-;
-
-EXEC
-;
-
-// Eights batch
-EXEC
-	ALU:
-		ALU0:	MAD  lp.lh, r0.l, #1, -r2.l
-		ALU1:	MAD  lp.lh, r0.h, #1, -r2.h
-		ALU2:	MAD  lp.lh, r1.l, #1, -r3.l
-		ALU3:	MAD  lp.lh, r1.h, #1, -r3.h
-
-	ALU:
-		ALU0:	MAD  lp.lh, abs(alu0), #1, #0 (this)
-		ALU1:	MAD  lp.lh, abs(alu1), #1, #0 (other)
-		ALU2:	MAD  lp.lh, abs(alu2), #1, #0 (other)
-		ALU3:	MAD  lp.lh, abs(alu3), u8.l, #0
-
-	// kill the pixel if dst is unchanged
-	ALU:
-		ALU0:	CSEL kill, -alu0, #0, #1
-		ALU1:	CSEL r1.h, -u8.l, r1.h, #0
 
 	DW:	store rt1, r0, r1
 ;
