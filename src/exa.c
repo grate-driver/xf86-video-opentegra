grate-driver/xf86-video-opentegra

commit bca9771a753dd2eadda99e615f97ccd08b377129

exa: optimize pixmaps freeing on grate-kernel
GRATE-kernel supports BO reservations, which are already supported by Opentegra.
Hence there is no need to block on BO freeing.

src/exa.c
@@ -231,6 +231,7 @@ static void TegraEXATrimHeap(TegraEXAPtr exa)
 static void TegraEXAReleasePixmapData(TegraPtr tegra, TegraPixmapPtr priv)
 {
     TegraEXAPtr exa = tegra->exa;
+    int drm_ver;
 
     if (priv->type == TEGRA_EXA_PIXMAP_TYPE_NONE) {
         if (priv->frozen) {
@@ -260,20 +261,24 @@ static void TegraEXAReleasePixmapData(TegraPtr tegra, TegraPixmapPtr priv)
         goto out_final;
     }
 
+    drm_ver = drm_tegra_version(tegra->drm);
+
     /*
      * We have to await the fence to avoid BO re-use while job is in progress,
      * this will be resolved by BO reservation that right now isn't supported
-     * by kernel driver.
+     * by vanilla upstream kernel driver.
      */
     if (priv->fence_read) {
-        TegraEXAWaitFence(priv->fence_read);
+        if (drm_ver < GRATE_KERNEL_DRM_VERSION)
+            TegraEXAWaitFence(priv->fence_read);
 
         tegra_stream_put_fence(priv->fence_read);
         priv->fence_read = NULL;
     }
 
     if (priv->fence_write) {
-        TegraEXAWaitFence(priv->fence_write);
+        if (drm_ver < GRATE_KERNEL_DRM_VERSION)
+            TegraEXAWaitFence(priv->fence_write);
 
         tegra_stream_put_fence(priv->fence_write);
         priv->fence_write = NULL;

commit 86be61f9e7a524345feacfb4ea16c92941c56f0e

exa: always fence pool-allocated pixmaps on release
There is no way for a pool allocation to check the BO-usage status at
allocation time, so we actually always need to fence them.

Fixes: bca9771a753dd2eadda99e615f97ccd08b377129

src/exa.c
@@ -265,7 +265,6 @@ static void TegraEXAReleasePixmapData(TegraPtr tegra, TegraPixmapPtr priv)
         goto out_final;
     }
 
-#ifdef POOL_DEBUG_CANARY
     /*
      * Pool allocation data is sprayed with 0x88 if canary-debugging is
      * enabled, see mem_pool_free(). In this case we need to enforce
@@ -275,9 +274,12 @@ static void TegraEXAReleasePixmapData(TegraPtr tegra, TegraPixmapPtr priv)
      * One example where problem is visible is a "magnus" application of
      * MATE DE, click on the magnus itself to see the corrupted image of
      * the checkerboard background that app draws.
+     *
+     * Secondly, we can't check the pool allocation HW usage status at
+     * allocation time, so we always need to fence a such allocation.
      */
     force_fencing = (priv->type == TEGRA_EXA_PIXMAP_TYPE_POOL);
-#endif
+
     drm_ver = drm_tegra_version(tegra->drm);
 
     /*